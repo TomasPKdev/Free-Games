858e9b86a608bf0d01092f3a30f4f4a4b28a19b6
PiousLoki <tomasvallejosroig@gmail.com>
2026-03-04 23:56:24 -0300

    steam corrections

--- steam.js
+++ steam.js
@@ -1,28 +1,31 @@
-fetch("https://corsproxy.io/?https://store.steampowered.com/api/featuredcategories/?cc=AR&l=spanish")
+fetch("https://corsproxy.io/?https://store.steampowered.com/search/results/?maxprice=free&specials=1&json=1")
 .then(respuesta => respuesta.json())
 .then(datos => {
-    const elementos = datos.specials.items;
+    const elementos = datos.items;
     console.log(elementos);
-    const juegosFree = elementos.filter(juego => {
-    return juego.discount_percent === 100 && juego.discounted === true;
-    });
-    if (juegosFree.length === 0) {
-    const mensaje = document.createElement("p");
-    mensaje.textContent = "No hay juegos gratis disponibles en este momento.";
-    document.querySelector(".games-grid").appendChild(mensaje);
-    return;
+    if (!elementos || elementos.length === 0) {
+        const mensaje = document.createElement("p");
+        mensaje.textContent = "No hay juegos gratis disponibles en este momento.";
+        document.querySelector(".games-grid").appendChild(mensaje);
+        return;
     }
-    juegosFree.forEach(element => {
-        const grid = document.querySelector(".games-grid")
-        const card =document.createElement("article");
-        card.className = "card-h";
-        const p = document.createElement ("p");
-        p.textContent = element.title;
-        const img = document.createElement ("img");
-        img.src = element.large_capsule_image;
-        grid.appendChild(card)
-        card.appendChild(p)
-        card.appendChild(img)
-        console.log(datos)
-    });    
+    elementos.forEach(element => {
+        const appid = element.logo.match(/apps\/(\d+)\//)[1];
+        fetch(`https://corsproxy.io/?https://store.steampowered.com/api/appdetails?appids=${appid}`)
+        .then(r => r.json())
+        .then(detalle => {
+            const juego = detalle[appid].data;
+            const grid = document.querySelector(".games-grid")
+            const card = document.createElement("article");
+            card.className = "card-h";
+            const p = document.createElement("p");
+            p.textContent = element.name;
+            const img = document.createElement("img");
+            img.src = juego.capsule_imagev5;
+            grid.appendChild(card)
+            card.appendChild(p)
+            card.appendChild(img)
+            console.log(juego)
+        });
     });
+});
